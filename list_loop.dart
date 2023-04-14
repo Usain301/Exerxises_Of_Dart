3f51a6b67ba83f8f4121fc4d38b977b83b7571d0
Usain301 <73827957+Usain301@users.noreply.github.com>
2023-04-15 02:37:43 +0300

    Update list_loop.dart

--- list_loop.dart
+++ list_loop.dart
@@ -1,3 +1,17 @@
 void main(List<String> args) {
-  
+  List<int> a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+
+  // for (var element in a) {
+  //   if (element < 5) {
+  //     print(element);
+  //   }
+
+  // }
+
+  //2
+  for (var i = 0; i < a.length; i++) {
+    if (a[i] < 5) {
+      print(a[i]);
+    }
+  }
 }
